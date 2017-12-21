3102dc98e0a6561a4d822f0930ebd6a2f2b9b534
Zhanna Sydorovych <janevanilla92@gmail.com>
2017-12-21 13:54:42 +0300

    add adaptive nav

--- main.js
+++ main.js
@@ -34,20 +34,18 @@ function myFunction2() {
 
 var hamburger = document.querySelector(".top-content__nav-hamburger");
 var navigation = document.querySelector(".page-nav__list");
-var showBtn = document.querySelector(".page-nav__show");
-var closeBtn = document.querySelector(".page-nav__close");
+var closeBtn = document.querySelector(".top-content__close-hamburger");
 
 hamburger.addEventListener("click", function(event1) {
    event1.preventDefault();
    navigation.classList.add("page-nav__show");
-   hamburger.classList.add("page-nav__close");
    closeBtn.classList.add("page-nav__show");
+   hamburger.classList.add("page-nav__close");
 });
-/*
-closeBtn.addEventListener("click", function(event2) {
-    event2.preventDefault();
+
+closeBtn.addEventListener("click", function(event) {
+    event.preventDefault();
     navigation.classList.remove("page-nav__show");
-    hamburger.classList.add("page-nav__show");
-    closeBtn.classList.remove("page-nav__show")
+    closeBtn.classList.remove("page-nav__show");
     hamburger.classList.remove("page-nav__close");
-});*/
+});
